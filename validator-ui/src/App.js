cd0917b5b04b28fa26d5374dd16009c2a0488479
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-01-24 00:17:26 +0200

    Add routing and state management to App.js and index.js

--- validator-ui/src/App.js
+++ validator-ui/src/App.js
@@ -1,8 +1,26 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
+import Logout from "./pages/logout";
+import Oauth from "./pages/oauth";
+import AppLayout from "./layouts/appLayout";
+import ExternalRedirect from "./pages/externalRedirect";
 
 function App() {
-  return <Homepage />;
+  const login_url = process.env.REACT_APP_LOGIN_URL;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/logout" element={<Logout />} />
+        </Route>
+
+        <Route path="/oauth/*" element={<Oauth />} />
+        <Route path="/login" element={<ExternalRedirect to={login_url} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
