bff4e20d60594cb03197b6ac897bdd350348c665
dehelean-catalin <drcatalin00@gmail.com>
2024-02-01 23:57:34 +0200

    Init tests

--- validator-ui/src/services/AxiosInterceptors.jsx
+++ validator-ui/src/services/AxiosInterceptors.jsx
@@ -4,65 +4,16 @@ import { PRIVATE_API } from './Api';
 import { updateExpiredAccessToken } from './auth/auth.service';
 
 export function AxiosInterceptors({ children }) {
-    const { logout, refreshToken, updateAccessToken } = useAuthContext();
+    const authContext = useAuthContext();
 
     useEffect(() => {
         let isRefreshing = false;
         const refreshAndRetryQueue = [];
 
-        const respInterceptor = PRIVATE_API.interceptors.response.use(
-            (response) => response,
-            async function unauthorizedResponseInterceptor(error) {
-                const originalRequest = error.config;
-
-                if (error?.response?.status === 403) {
-                    if (isRefreshing) {
-                        isRefreshing = false;
-                        logout();
-                        return Promise.reject(error);
-                    }
-
-                    isRefreshing = true;
-
-                    try {
-                        if (!refreshToken) {
-                            throw new Error('Invalid token');
-                        }
-
-                        // In case a request fails, we need to memoized the originalRequest in order preserve the retry property
-                        // and to not cause an infinite loop
-
-                        const response = await updateExpiredAccessToken(refreshToken);
-
-                        if (!response.access) {
-                            throw new Error('Invalid token');
-                        }
-
-                        updateAccessToken(response.access);
-                        error.config.headers['Authorization'] = `Bearer ${response.access}`;
-
-                        refreshAndRetryQueue.forEach(({ config, resolve, reject }) => {
-                            PRIVATE_API.request(config)
-                                .then((response) => resolve(response))
-                                .catch((err) => reject(err));
-                        });
-
-                        refreshAndRetryQueue.length = 0;
-
-                        return PRIVATE_API(originalRequest);
-                    } catch (error) {
-                        logout();
-                    } finally {
-                        isRefreshing = false;
-                    }
-
-                    return new Promise((resolve, reject) => {
-                        refreshAndRetryQueue.push({ config: originalRequest, resolve, reject });
-                    });
-                }
-
-                return Promise.reject(error);
-            }
+        const respInterceptor = authResponseInterceptor(
+            isRefreshing,
+            refreshAndRetryQueue,
+            authContext
         );
 
         return () => {
@@ -71,4 +22,63 @@ export function AxiosInterceptors({ children }) {
     }, []);
 
     return children;
+}
+
+export function authResponseInterceptor(isRefreshing, refreshAndRetryQueue, authContext) {
+    const { refreshToken, logout, updateAccessToken } = authContext;
+
+    return PRIVATE_API.interceptors.response.use(
+        (response) => response,
+        async function unauthorizedResponseInterceptor(error) {
+            const originalRequest = error.config;
+
+            if (error?.response?.status === 403) {
+                if (isRefreshing) {
+                    isRefreshing = false;
+                    logout();
+                    return Promise.reject(error);
+                }
+
+                isRefreshing = true;
+
+                try {
+                    if (!refreshToken) {
+                        throw new Error('Invalid token');
+                    }
+
+                    // In case a request fails, we need to memoized the originalRequest in order preserve the retry property
+                    // and to not cause an infinite loop
+
+                    const response = await updateExpiredAccessToken(refreshToken);
+
+                    if (!response.access) {
+                        throw new Error('Invalid token');
+                    }
+
+                    updateAccessToken(response.access);
+                    error.config.headers['Authorization'] = `Bearer ${response.access}`;
+
+                    refreshAndRetryQueue.forEach(({ config, resolve, reject }) => {
+                        PRIVATE_API.request(config)
+                            .then((response) => resolve(response))
+                            .catch((err) => reject(err));
+                    });
+
+                    refreshAndRetryQueue.length = 0;
+
+                    return PRIVATE_API(originalRequest);
+                } catch (error) {
+                    logout();
+                } finally {
+                    isRefreshing = false;
+                }
+
+                return new Promise((resolve, reject) => {
+                    refreshAndRetryQueue.push({ config: originalRequest, resolve, reject });
+                });
+            }
+
+            return Promise.reject(error);
+        }
+    );
 }
---
+++ validator-ui/vite.config.js
@@ -0,0 +1,9 @@
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {},
+});
