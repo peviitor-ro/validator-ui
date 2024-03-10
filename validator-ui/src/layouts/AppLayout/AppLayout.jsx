f5609be3173a71b3551dc67c8979e77aec59ea76
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-03-10 21:08:48 +0200

    Update UI layout and links

--- validator-ui/src/layouts/AppLayout/AppLayout.jsx
+++ validator-ui/src/layouts/AppLayout/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Header } from './components/Header';
 
 export function AppLayout() {
     return (
-        <Container>
+        <Container className={"bg-background"}>
             <Header />
             <Outlet />
         </Container>
