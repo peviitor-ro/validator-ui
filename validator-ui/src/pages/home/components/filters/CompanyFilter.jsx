6cb6d1e249cb7bfa6af406e54c9b826f5a808940
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-07-23 18:45:53 +0300

    chore: Refactor landing service and add company form modal

--- validator-ui/src/pages/home/components/filters/CompanyFilter.jsx
+++ validator-ui/src/pages/home/components/filters/CompanyFilter.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 import { useCompanyOptionsSelector } from '../../../../store/company.selectors';
@@ -5,6 +7,9 @@ import { SORT_OPTIONS } from './constants';
 
 import { InputField } from '../../../../components/InputField/InputField';
 import { SelectField } from '../../../../components/SelectField';
+import { Button } from '../../../../components/Button';
+import { Modal } from '../../../../components/Modal';
+import { CompanyForm } from '../forms/CompanyForm';
 
 export function Home({ children }) {
     return <main className="flex flex-col gap-4 p-4 lg:gap-10 lg:p-10">{children}</main>;
@@ -12,14 +17,12 @@ export function Home({ children }) {
 
 Home.Header = function H() {
     const { search, order, setOrder, setSearch } = useCompanyOptionsSelector();
+    const [open, setOpen] = useState(false);
 
     return (
         <div
             id="search-container"
-            className="flex flex-col gap-2 
-            border-b border-disabled pb-4 lg:flex-row lg:pb-6 lg:items-center 
-
-            "
+            className="flex flex-col gap-2 border-b border-disabled pb-4 lg:flex-row lg:pb-6 lg:items-center"
         >
             <h1 id="companies-title" className="text-3xl font-semibold text-primary">
                 Companii
@@ -35,6 +38,14 @@ Home.Header = function H() {
                 showError={false}
             />
             <SelectField options={SORT_OPTIONS} value={order} onChange={setOrder} />
+
+            <Button className="w-full lg:w-auto" text="Adaugă"
+                onClick={() => setOpen(true)}
+            />
+
+            <Modal open={open} setOpen={setOpen} title="Adaugă companie" className="w-11/12 md:w-2/3">
+                <CompanyForm />
+            </Modal>
         </div>
     );
 };
