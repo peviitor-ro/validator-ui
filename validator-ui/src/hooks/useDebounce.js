afb38cbffd96ab51c79de1e320d92c372d094682
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-09-26 02:09:37 +0300

    Refactor infiniteScroll hook for improved code structure and readability

--- validator-ui/src/hooks/useDebounce.js
+++ validator-ui/src/hooks/useDebounce.js
@@ -1,5 +1,15 @@
 import { useEffect, useState } from 'react';
 
+/**
+ * Custom hook that debounces a value by a specified delay.
+ *
+ * @param {any} value - The value to debounce.
+ * @param {number} [delay=500] - The delay in milliseconds for debouncing.
+ * @returns {any} - The debounced value.
+ *
+ * @example
+ * const debouncedSearchTerm = useDebounce(searchTerm, 300);
+ */
 export function useDebounce(value, delay = 500) {
     const [debouncedValue, setDebouncedValue] = useState(value);
 
