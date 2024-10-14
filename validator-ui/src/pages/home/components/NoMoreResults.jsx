14e6257c34ba578e4d234a4edd68bda885c81520
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-10-14 04:05:33 +0300

    Refactor NoMoreResults component and update UI

--- validator-ui/src/pages/home/components/NoMoreResults.jsx
+++ validator-ui/src/pages/home/components/NoMoreResults.jsx
@@ -1,15 +1,17 @@
 import Globe from '../../../components/ui/globe';
 
+/**
+ * NoMoreResults component displays a message indicating that there are no more results.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.message - The message to display.
+ * @returns {JSX.Element} The rendered component.
+ */
 export function NoMoreResults({ message }) {
     return (
         <div className="flex flex-col justify-center items-center">
             <Globe className="relative mx-auto right-0 w-40" />
-            <span
-                className="text-sm font-semibold text-gray-500
-            "
-            >
-                {message}
-            </span>
+            <span className="text-sm font-semibold text-gray-500">{message}</span>
         </div>
     );
 }
